415db9def6df6dbc5796997f0ae0cb61a3da844a
ZacHolme7 <zacholme@gmail.com>
2024-08-01 18:02:51 -0500

    bitmap snapshot work

--- contracts/src/V3TickBitmapUpdate.sol
+++ contracts/src/V3TickBitmapUpdate.sol
@@ -1,41 +1,37 @@
-//SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
-
 
 interface IUniswapV3Pool {
     function tickBitmap(int16 wordPosition) external view returns (uint256);
+    function slot0() external view returns (uint160 sqrtPriceX96, int24 tick, uint16 observationIndex, uint16 observationCardinality, uint16 observationCardinalityNext, uint8 feeProtocol, bool unlocked);
 }
 
 contract V3TickBitmapUpdate {
     struct V3TickBitmapData {
         address poolAddr;
-        uint256[7] tickBitmaps;  // Fixed-length array of 7 elements
+        uint256[7] tickBitmaps; // Fixed-length array of 7 elements
     }
 
-    function getTickBitmaps(address[] memory pools, int24[] memory currentTicks) 
-        external 
-        view 
-        returns (V3TickBitmapData[] memory) 
-    {
+    constructor(address[] memory pools) {
         V3TickBitmapData[] memory allPoolData = new V3TickBitmapData[](pools.length);
-        
+
         for (uint256 i = 0; i < pools.length; ++i) {
             address poolAddress = pools[i];
-            int24 currentTick = currentTicks[i];
-            int16 wordPosition = int16(currentTick / 256);  // Cast to int16 for tickBitmap function
-            
-            V3TickBitmapData memory poolData;
-            poolData.poolAddr = poolAddress;
+            (, int24 currentTick,,,,,) = IUniswapV3Pool(poolAddress).slot0();
+            int16 wordPosition = int16(currentTick >> 8); // Equivalent to dividing by 256
 
+            allPoolData[i].poolAddr = poolAddress;
             // Get tickBitmaps: 3 before, current, and 3 after
-            for(int16 j = -3; j <= 3; j++) {
-                uint256 bitmap = IUniswapV3Pool(poolAddress).tickBitmap(wordPosition + j);
-                poolData.tickBitmaps[uint256(j + 3)] = bitmap;
+            for (int256 j = -3; j <= 3; j++) {
+                uint256 bitmap = IUniswapV3Pool(poolAddress).tickBitmap(wordPosition + int16(j));
+                allPoolData[i].tickBitmaps[uint256(j + 3)] = bitmap;
             }
-
-            allPoolData[i] = poolData;
         }
 
-        return allPoolData;
+        bytes memory _abiEncodedData = abi.encode(allPoolData);
+        assembly {
+            let dataStart := add(_abiEncodedData, 0x20)
+            return(dataStart, sub(msize(), dataStart))
+        }
     }
 }
